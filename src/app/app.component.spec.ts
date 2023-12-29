22af65921f96e5dec4c4410990962c9d93a283f3
AssemToumi <assamtomi@gmail.com>
2023-12-29 23:23:13 +0100

    PR25

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'YourFrontendProjectName' title`, () => {
+  it(`should have the 'FrontendProject' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('YourFrontendProjectName');
+    expect(app.title).toEqual('FrontendProject');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, YourFrontendProjectName');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, FrontendProject');
   });
 });
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+// environment.prod.ts
+
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.example.com/prod',
+  // Add other production-specific configurations here
+};
