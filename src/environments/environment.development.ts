3dfdabf9368ef5f05142bc408b3218c2ca84c5db
AssemToumi <63657019+AssemToumi@users.noreply.github.com>
2023-12-29 23:47:23 +0100

    Pr25 (#14)
    * PR24
    
    * F
    
    * M
    
    * PR25
    
    * O

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://api.example.com/development',
+    // Add other development-specific configurations here
+  };
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,7 @@
+// environment.staging.ts
+
+export const environment = {
+  production: false,
+  apiUrl: 'https://api.example.com/staging',
+  // Add other staging-specific configurations here
+};
